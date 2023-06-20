9b9ee3949aaf7140ac0bb098d621b037d3b28b43
zhe <zhesolworks@gmail.com>
2023-06-20 20:51:23 +0100

    Update logo

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,6 +5,7 @@ import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
+import Image from "next/image"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -14,7 +15,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
+        <Image src={'https://openmoji.org/data/color/svg/E20B.svg'} alt={siteConfig.name} width={32} height={32} />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
